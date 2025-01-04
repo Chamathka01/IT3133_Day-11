0334b1fae78fe83d983c4b444f3751936f356398
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-04 10:10:23 +0000

    Add useState and navigation for ContactUs,js

--- StudentCareApp/components/ContactUs.js
+++ StudentCareApp/components/ContactUs.js
@@ -1,5 +1,8 @@
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 export default function ContactUs() {
     return(
-
+        const [name, setName] = useState();
+        const navigation = useNavigation();
     );
 }
