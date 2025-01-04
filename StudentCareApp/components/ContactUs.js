6f728be6505cb2ff56010df8001e3f34738c24ae
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-04 10:12:24 +0000

    Display headline

--- StudentCareApp/components/ContactUs.js
+++ StudentCareApp/components/ContactUs.js
@@ -1,8 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 export default function ContactUs() {
+    const [name, setName] = useState();
+    const navigation = useNavigation();
     return(
-        const [name, setName] = useState();
-        const navigation = useNavigation();
+        <PaperProvider>
+            <View style={styles.container}>
+                 <View style={styles.header}>
+                     <Text variant="headlineLarge">Contact Us</Text>
+                    <Divider />
+                 </View>
+        </PaperProvider>
+        
     );
 }
