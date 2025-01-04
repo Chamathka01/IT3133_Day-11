91323988eecbd3385830bb5b57f0bdd6eda8f815
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-04 10:41:45 +0000

    Importing statements

--- StudentCareApp/App.js
+++ StudentCareApp/App.js
@@ -1,6 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './components/Home';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 export default function App() {
   return (
     <View style={styles.container}>
