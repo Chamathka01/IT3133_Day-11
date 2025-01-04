6ede04f9556e3b472de7cd23cf0743b16ba755a6
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-01-04 10:33:38 +0000

    Add image for AboutUs.js

--- StudentCareApp/components/AboutUs.js
+++ StudentCareApp/components/AboutUs.js
@@ -3,6 +3,9 @@ export default function AboutUs() {
         <PaperProvider>
             <Text variant="headlineLarge">History</Text>
             <Divider />
+            <View style={styles.imagepad}>
+                <Image source={require('../assets/uovlogo.png')} style={styles.image}/>
+            </View>
         </PaperProvider>
     );
 }
